24c669d20c5523248f52bb95be1c64009cca03c8
Bogdan Teslarasu <bogdantsu@yahoo.com>
2015-10-07 23:54:54 +0300

    Finished exercise 1 of 5th homework.

--- src/ConnectedComponentsComputation.java
+++ src/ConnectedComponentsComputation.java
@@ -22,9 +22,60 @@ public class ConnectedComponentsComputation extends
    * @throws IOException
    */
   @Override
-  public void compute(
-      Vertex<IntWritable, IntWritable, NullWritable> vertex,
-      Iterable<IntWritable> messages) throws IOException {
-      //TODO
+  public void compute(Vertex<IntWritable, IntWritable, NullWritable> vertex,
+					  Iterable<IntWritable> messages) throws IOException {
+	  if (getSuperstep() == 0){
+		  computeSuperStep(vertex);
+	  }else{
+		  computeRegularStep(vertex, messages);
+	  }
+	  vertex.voteToHalt();
   }
+
+	private void computeSuperStep(Vertex<IntWritable, IntWritable, NullWritable> vertex) {
+		int minVertex = findMinVertex(vertex);
+		if (vertex.getValue().get() != minVertex){
+			vertex.setValue(new IntWritable(minVertex));
+			propagateVertex(vertex);
+		}
+	}
+
+	private int findMinVertex(Vertex<IntWritable, IntWritable, NullWritable> vertex) {
+		int minVertex = vertex.getValue().get();
+		for(Edge<IntWritable, NullWritable> edge: vertex.getEdges()){
+			int neighbour = edge.getTargetVertexId().get();
+			if (neighbour < minVertex){
+				minVertex = neighbour;
+			}
+		}
+		return minVertex;
+	}
+
+	private void propagateVertex(Vertex<IntWritable, IntWritable, NullWritable> vertex) {
+		for(Edge<IntWritable, NullWritable> edge: vertex.getEdges()){
+			int neighbour = edge.getTargetVertexId().get();
+			if (vertex.getValue().get() < neighbour){
+				sendMessage(edge.getTargetVertexId(), vertex.getValue());
+			}
+		}
+	}
+
+	private void computeRegularStep(Vertex<IntWritable, IntWritable, NullWritable> vertex,
+									Iterable<IntWritable> messages) {
+		int minId = findMinId(messages);
+		if (minId < vertex.getValue().get()){
+			vertex.setValue(new IntWritable(minId));
+			sendMessageToAllEdges(vertex, vertex.getValue());
+		}
+	}
+
+	private int findMinId(Iterable<IntWritable> collection){
+		int min = Integer.MAX_VALUE;
+		for (IntWritable item: collection){
+			if (item.get() < min){
+				min = item.get();
+			}
+		}
+		return min;
+	}
 }
