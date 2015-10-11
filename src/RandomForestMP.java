6a2dfc9330ae936b4c037a8fcb9dc2f5aa1eac35
Bogdan Teslarasu <bogdantsu@yahoo.com>
2015-10-11 23:43:59 +0300

    Finished exercise 4 of Homework 5

--- src/RandomForestMP.java
+++ src/RandomForestMP.java
@@ -2,19 +2,40 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.mllib.classification.SVMModel;
-import org.apache.spark.mllib.classification.SVMWithSGD;
-import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.regression.LabeledPoint;
-import org.apache.spark.mllib.tree.model.RandomForestModel;
 import org.apache.spark.mllib.tree.RandomForest;
-
+import org.apache.spark.mllib.tree.model.RandomForestModel;
 
 import java.util.HashMap;
-import java.util.regex.Pattern;
 
 public final class RandomForestMP {
+
+	private static class ExtractFeatures implements Function<String, LabeledPoint>{
+		@Override
+		public LabeledPoint call(String line) throws Exception {
+			String[] points = line.split(",");
+			double[] features = new double[points.length-1];
+			for (int i = 0; i < points.length - 1; i++){
+				features[i] = Double.valueOf(points[i]);
+			}
+			return new LabeledPoint(Double.valueOf(points[points.length-1]), Vectors.dense(features));
+		}
+	}
+
+	private static class ExtractPredictions implements Function<LabeledPoint, LabeledPoint>{
+		RandomForestModel model;
+		public ExtractPredictions(RandomForestModel model){
+			this.model = model;
+		}
+
+		@Override
+		public LabeledPoint call(LabeledPoint input) throws Exception {
+			Vectors.dense(input.label(), input.features().toArray());
+			return new LabeledPoint(model.predict(input.features()),
+							Vectors.dense(input.label(), input.features().toArray()));
+		}
+	}
 
     public static void main(String[] args) {
         if (args.length < 3) {
@@ -31,7 +52,7 @@ public final class RandomForestMP {
         final RandomForestModel model;
 
         Integer numClasses = 2;
-        HashMap<Integer, Integer> categoricalFeaturesInfo = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> categoricalFeaturesInfo = new HashMap<>();
         Integer numTrees = 3;
         String featureSubsetStrategy = "auto";
         String impurity = "gini";
@@ -40,15 +61,13 @@ public final class RandomForestMP {
         Integer seed = 12345;
 
 		// TODO
+		JavaRDD<LabeledPoint> trainData = sc.textFile(training_data_path).map(new ExtractFeatures());
+		JavaRDD<LabeledPoint> test = sc.textFile(test_data_path).map(new ExtractFeatures());
+		model = RandomForest.trainClassifier(trainData, numClasses, categoricalFeaturesInfo, numTrees,
+					 featureSubsetStrategy, impurity, maxDepth, maxBins, seed);
 
-        /*JavaRDD<LabeledPoint> results = test.map(new Function<Vector, LabeledPoint>() {
-            public LabeledPoint call(Vector points) {
-                return new LabeledPoint(model.predict(points), points);
-            }
-        });
-
-        results.saveAsTextFile(results_path);*/
-
+        JavaRDD<LabeledPoint> results = test.map(new ExtractPredictions(model));
+        results.saveAsTextFile(results_path);
         sc.stop();
     }
 
